4c89f9fdb6f9c1ef41bf411b084d614a29fd4b9e
Jefferson Moraes <jeffersonjuniormoraes@gmail.com>
2023-10-18 11:30:09 -0300

    :tada: First Commit

---
+++ src/main/java/br/com/serragram/serragram/SerragramApplication.java
@@ -0,0 +1,13 @@
+package br.com.serragram.serragram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerragramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SerragramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/serragram/serragram/SerragramApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.serragram.serragram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerragramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
