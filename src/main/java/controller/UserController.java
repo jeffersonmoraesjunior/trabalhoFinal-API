31eb7b4b220e4b44d4755826253141fa41d33fc9
Jefferson Moraes <jeffersonjuniormoraes@gmail.com>
2023-10-18 19:05:32 -0300

    Algumas Classes Criadas com o Grupo Juntamente (User)

---
+++ src/main/java/controller/UserController.java
@@ -0,0 +1,30 @@
+package controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import DTO.UserDTO;
+import service.UserService;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+	
+	@Autowired
+	private UserService userService;
+	
+	@GetMapping
+	public ResponseEntity<List<UserDTO>> listar() {
+		
+		return null;
+	}
+	
+	
+	
+
+}
---
+++ src/main/java/model/User.java
@@ -0,0 +1,92 @@
+package model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "user")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_user")
+	private Long id;
+	
+	@NotBlank(message = "Preencha o nome")
+	@Column(length = 60, nullable = false)
+	private String nome;
+	
+	@NotBlank(message = "Preencha o Sobrenome")
+	@Column(length = 60, nullable = false)
+	private String sobreNome;
+	
+	@NotBlank(message = "Preencha o Email")
+	@Email(message = "Preencha o Email Válido")
+	@Column(length = 60, nullable = false)
+	private String email;
+	
+	@NotBlank(message = "Preencha a Senha")
+	@Column(length = 60, nullable = false)
+	private String senha;
+	
+	@NotBlank(message = "Preencha a data de Nascimento")
+	@Column(length = 60, nullable = false)
+	private Date dataNascimento;
+	
+	public User() {
+	
+	}
+	public User(Long id, String nome, String sobreNome, String email, String senha, Date dataNascimento) {
+		this.id = id;
+		this.nome = nome;
+		this.sobreNome = sobreNome;
+		this.email = email;
+		this.senha = senha;
+		this.dataNascimento = dataNascimento;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSobreNome() {
+		return sobreNome;
+	}
+	public void setSobreNome(String sobreNome) {
+		this.sobreNome = sobreNome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}	
+
+}
