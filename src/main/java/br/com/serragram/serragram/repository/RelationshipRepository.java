268d9532a6da112e680892319f7ed59ca5abfed3
danilofariaspereira <danilofariaspereira90@gmail.com>
2023-10-21 19:25:19 -0300

    criando lista de seguidores

--- src/main/java/br/com/serragram/serragram/repository/RelationshipRepository.java
+++ src/main/java/br/com/serragram/serragram/repository/RelationshipRepository.java
@@ -2,10 +2,12 @@ package br.com.serragram.serragram.repository;
 
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import br.com.serragram.serragram.DTO.RelationshipDTO;
 import br.com.serragram.serragram.model.Relationship;
 
 @Repository
@@ -20,4 +22,7 @@ public interface RelationshipRepository extends JpaRepository<Relationship, Long
 	//Optional<Relationship> deleteRelationship(Long seguidorId, Long seguidoId);
 	
 	void deleteByIdUserSeguidorIdAndIdUserSeguidoId(Long seguidorId, Long seguidoId);
+	
+	@Query(value = "select * from relationship WHERE id_user_seguido = 1", nativeQuery = true)
+	 Optional<Page<RelationshipDTO>> buscarSeguidores();
 }
